079c7a6f0ff1fbf84308ce22a1f691262f1db8ea
Eddie <eddiegabriel4@gmail.com>
2021-05-28 20:41:52 -0700

    i

--- concordance_tests.py
+++ concordance_tests.py
@@ -1,14 +1,16 @@
 import unittest
 from concordance import *
+from hash_quad import *
 
 class TestList(unittest.TestCase):
 
+    
     def test_01(self) -> None:
         conc = Concordance()
         conc.load_stop_table("stop_words.txt")
         conc.load_concordance_table("file1.txt")
         conc.write_concordance("file1_con.txt")
-        self.assertTrue(compare_files("file1_con.txt", "file1_sol.txt"))
+        self.assertTrue(compare_files("file1_con.txt", "file1_sol.txt")) 
 
     def test_02(self) -> None:
         conc = Concordance()
@@ -16,13 +18,21 @@ class TestList(unittest.TestCase):
         conc.load_concordance_table("file2.txt")
         conc.write_concordance("file2_con.txt")
         self.assertTrue(compare_files("file2_con.txt", "file2_sol.txt"))
-
+    
     def test_03(self) -> None:
         conc = Concordance()
         conc.load_stop_table("stop_words.txt")
         conc.load_concordance_table("declaration.txt")
         conc.write_concordance("declaration_con.txt")
         self.assertTrue(compare_files("declaration_con.txt", "declaration_sol.txt"))
+
+    def test_error(self) -> None:
+        conc = Concordance()
+        with self.assertRaises(FileNotFoundError):
+            conc.load_stop_table('afsjkdhfoukash.txt')
+            conc.load_concordance_table('asdjhfkjashdfkjshad.txt')
+
+
 
 # Compare files - takes care of CR/LF, LF issues
 def compare_files(file1: str, file2: str) -> bool:
@@ -41,4 +51,4 @@ def compare_files(file1: str, file2: str) -> bool:
     return match
 
 if __name__ == '__main__':
-   unittest.main(warnings="ignore")
+    unittest.main(warnings="ignore")
